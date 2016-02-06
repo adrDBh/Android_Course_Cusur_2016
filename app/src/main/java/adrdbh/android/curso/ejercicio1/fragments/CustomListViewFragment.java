0895bbed4648a2053639dbb00eea8c048f17f9ba
Adrián <adr.100@hotmail.com>
2016-02-05 20:03:36 -0600

    EndSession, 05 Friday, Feb 16'

---
+++ app/src/main/java/adrdbh/android/curso/ejercicio1/fragments/CustomListViewFragment.java
@@ -0,0 +1,29 @@
+package adrdbh.android.curso.ejercicio1.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import adrdbh.android.curso.ejercicio1.R;
+
+/**
+ * Created by adrdbh on 2/5/16.
+ */
+public class CustomListViewFragment extends Fragment {
+
+    public static CustomListViewFragment newInstance() {
+        CustomListViewFragment customListViewFragment = new CustomListViewFragment();
+
+        return customListViewFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.custom_listview_fragment, container, false);
+        return view;
+    }
+}
--- app/src/main/java/adrdbh/android/curso/ejercicio1/fragments/ThreadFragment.java
+++ app/src/main/java/adrdbh/android/curso/ejercicio1/fragments/ThreadFragment.java
@@ -1,6 +1,7 @@
 package adrdbh.android.curso.ejercicio1.fragments;
 
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,13 +11,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import adrdbh.android.curso.ejercicio1.R;
 
 /*
  set ups the new threadfragment
 * */
-
 
 
 public class ThreadFragment extends Fragment {
@@ -51,6 +52,7 @@ public class ThreadFragment extends Fragment {
         mButtonWithoutThread.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // WEEEEEEE, we getting in that withoutThread method!!, see you there!
                 withoutThread();
             }
         });
@@ -58,13 +60,103 @@ public class ThreadFragment extends Fragment {
         mButtonWithThread.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //withThread();
+                withThreadOption2();
+
             }
         });
 
         return v;
     }
 
+    private void withThreadOption2() {
+        AsynTaskProgressBar ATP = new AsynTaskProgressBar();
+        ATP.execute();
+    }
+
+    // Creating a new asynchronous class from the android-made, way...
+    private class AsynTaskProgressBar extends AsyncTask<Void, Integer, Boolean> {
+
+        @Override
+        // Adding a background thread
+        protected Boolean doInBackground(Void... params) {
+            // Iteration though 0-100%
+            for (int i = 0; i <= 10; i++) {
+                SleepProcess();
+                // Icrement by multiples of 10
+                publishProgress(i * 10);
+                // If user leaves, or switches from fragment/activity, exit.
+                if (isCancelled()) {
+                    break;
+                }
+            }
+            return true;
+        }
+
+        @Override
+
+        protected void onProgressUpdate(Integer... values) {
+            int Progress = values[0].intValue();
+            mProgressBar.setProgress(Progress);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            mProgressBar.setMax(100);
+            mProgressBar.setProgress(0);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            if (aBoolean) {
+                Toast.makeText(getActivity(), "Hilo finalizado", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        protected void onCancelled() {
+            Toast.makeText(getActivity(), "Hilo finalizado :(", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void withThreadOption1() {
+        // Creates a new thread
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // post action for threaded progress bar needs a runnable
+                mProgressBar.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Set the progress again at 0
+                        mProgressBar.setProgress(0);
+                    }
+                });
+                // Iterate through 0-100% in the progress bar
+                for (int i = 0; i <= 10; i++) {
+                    // Sleep the program 1000 millis
+                    SleepProcess();
+                    // post method again, so new runnable needed
+                    mProgressBar.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Increment the progress bar for each iteration
+                            mProgressBar.incrementProgressBy(10);
+                        }
+                    });
+                }
+                // runs activity on the current window
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Sheesh, finally got out of Runnable-ception D:
+                        Toast.makeText(getActivity(), "Finished up wasting time :}", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+            // Finally we can start it...
+        }).start();
+    }
+
+    // Yo, you just ended up here!!
     private void withoutThread() {
         // Max value of progress bar (%)
         mProgressBar.setMax(100);
